9fbd6a55a703d2c33b0d6640fd06bf03f2ea3e89
Illusion0-0 <tarunmishra914@gmail.com>
2024-04-13 15:26:36 +0530

    service added with cache

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HttpClientModule } from  '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { GithubDataComponent } from './pages/github-data/github-data.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { SearchComponent } from './components/search/search.component';
+import { RepoCardComponent } from './components/repo-card/repo-card.component';
+
+const routes: Routes = [
+  {
+    path: ':username',
+    component: GithubDataComponent,
+    title: 'Github User',
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubDataComponent,
+    SearchComponent,
+    RepoCardComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,55 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CacheService } from './cache.service';
+import { IRepository } from 'src/models/repository.model';
+import { IUser } from 'src/models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
   constructor(
-    private httpClient: HttpClient
-  ) { }
+    private httpClient: HttpClient,
+    private cacheService: CacheService
+  ) {}
 
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  getUser(githubUsername: string): Observable<IUser> {
+    const cacheKey = `getUser:${githubUsername}`;
+    const cachedUser = this.cacheService.getFromCache<IUser>(cacheKey);
+
+    if (cachedUser) {
+      return of(cachedUser);
+    } else {
+      return this.httpClient
+        .get<IUser>(`https://api.github.com/users/${githubUsername}`)
+        .pipe(
+          tap((user) => this.cacheService.addToCache<IUser>(cacheKey, user))
+        );
+    }
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepositories(
+    githubUsername: string,
+    currentPage: number = 1,
+    perPage: number = 10
+  ): Observable<IRepository[]> {
+    const cacheKey = `getRepositories:${githubUsername}:${currentPage}:${perPage}`;
+    const cachedRepos = this.cacheService.getFromCache<IRepository[]>(cacheKey);
+
+    if (cachedRepos) {
+      return of(cachedRepos);
+    } else {
+      return this.httpClient
+        .get<IRepository[]>(
+          `https://api.github.com/users/${githubUsername}/repos?page=${currentPage}&per_page=${perPage}&sort=pushed`
+        )
+        .pipe(
+          tap((repos) =>
+            this.cacheService.addToCache<IRepository[]>(cacheKey, repos)
+          )
+        );
+    }
+  }
 }
